18a81254c7c85d917cd23dad069307b87347b570
Buhari Aminu Adam <buhareeameenu2@gmail.com>
2023-04-29 00:20:56 +0100

    learning

---
+++ Programming with JavaScript/Week 3/greeting.js
@@ -0,0 +1,10 @@
+
+export function formalGreeting(name){
+    console.log("Hello Mr " + name);
+}
+
+export function informalGreeting(name) {
+    console.log(`Hey ${name}, warrap man`)
+}
+
+export const greeting = console.log('Hello there');
